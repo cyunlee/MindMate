94e37c4c61d1f34ce218541255588b7ad02dda6f
KwonRam <kms971226@gmail.com>
2024-03-06 13:29:40 +0900

    9th commit

--- mind-mate/server/config/config.js
+++ mind-mate/server/config/config.js
@@ -19,7 +19,7 @@ const development = {
   username: 'admin',
   password: 'Cslikea%d10',
   database: 'mind_mate_db',
-  host: 'localhostdatabase-1.czmooi4c4wg8.ap-northeast-2.rds.amazonaws.com',
+  host: 'database-1.czmooi4c4wg8.ap-northeast-2.rds.amazonaws.com',
   dialect: 'mysql',
   port: 3306, // MySQL이 다른 포트에서 실행 중이면 이 부분을 수정하세요.
   timezone: '+09:00',
