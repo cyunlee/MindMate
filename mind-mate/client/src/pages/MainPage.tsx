e65f7d59aa65388fdbd31036b3873876e928cfa3
KwonRam <kms971226@gmail.com>
2024-03-07 09:39:25 +0900

    owo

--- mind-mate/client/src/pages/MainPage.tsx
+++ mind-mate/client/src/pages/MainPage.tsx
@@ -1,33 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      host: '',
-    };
-  }
-
-  componentDidMount() {
-    this._getHost();
-  }
-
-  _getHost = async () => {
-    const res = await axios.get('/api/host');
-    this.setState({ host: res.data.host });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <h3>
-          {' '}
-          Welcome to <u> {this.state.host} </u> Blog!{' '}
-        </h3>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div>Main</div>;
+};
 
 export default App;
