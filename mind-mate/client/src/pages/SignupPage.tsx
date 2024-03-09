8e34a2ec30cc06767a54eb71d8ab310647b6aa7c
cyunlee <siyunkr@gmail.com>
2024-03-09 13:16:23 +0900

    merge test

--- mind-mate/client/src/pages/SignupPage.tsx
+++ mind-mate/client/src/pages/SignupPage.tsx
@@ -1,10 +1,18 @@
 import axios from 'axios';
 import TopBar from '../components/TopBar';
+import '../styles/SignupPage.scss';
 
 function SignupPage() {
     return ( 
         <>
             <TopBar/>
+            <div className='signup-container'>
+                <div className='auth-title'>회원가입</div>
+                <div className='auth-content'>아이디</div>
+                <div className='auth-content'>비밀번호</div>
+                <div className='auth-content'>비밀번호 확인</div>
+                <div className='auth-content'>닉네임</div>
+            </div>
         </>
      );
 }
