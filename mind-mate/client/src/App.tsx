d7cc1cdeb065e59078ba7b26e2da45bd239e1022
KwonRam <kms971226@gmail.com>
2024-03-13 11:27:27 +0900

    socket

--- mind-mate/client/src/App.tsx
+++ mind-mate/client/src/App.tsx
@@ -1,24 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-//페이지 임포트
+import { Socket } from 'socket.io-client';
 import MainPage from './pages/MainPage';
 import Error404 from './pages/errors/Error404';
 import SignupPage from './pages/SignupPage';
 import LoginPage from './pages/LoginPage';
+import TestChat from './pages/TestChat';
 
-function App() {
+interface AppProps {
+  socket: Socket;
+}
+
+const App: React.FC<AppProps> = ({ socket }) => {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />}></Route>
-          <Route path="/signup" element={<SignupPage/>}></Route>
-          <Route path='/login' element={<LoginPage/>}></Route>
-          <Route path="*" element={<Error404 />}></Route>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/testchat/:roomId"
+            element={<TestChat socket={socket} />} // Pass the socket instance as a prop
+          />
+          <Route path="*" element={<Error404 />} />
         </Routes>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
---
+++ mind-mate/client/src/pages/TestChat.tsx
@@ -0,0 +1,64 @@
+// src/pages/TestChat.tsx
+import React, { useEffect, useRef, useState } from 'react';
+import { io, Socket } from 'socket.io-client'; // Import io function
+import { Link, useLocation, useNavigate, useParams } from 'react-router-dom';
+import axios from 'axios';
+interface TestChatProps {
+  socket: Socket;
+}
+
+const TestChat: React.FC<TestChatProps> = ({ socket }) => {
+  let { roomId } = useParams();
+  console.log('roomId', roomId);
+  const socketRef = useRef<Socket | null>(null);
+  const [messages, setMessages] = useState<string[]>([]);
+  useEffect(() => {
+    console.log('useEffect messages ', messages);
+  }, [messages]);
+  const [messageInput, setMessageInput] = useState('');
+
+  useEffect(() => {
+    socketRef.current = io('http://localhost:4000');
+
+    // Use optional chaining to handle null case
+    socketRef.current?.on('message', (message: string) => {
+      setMessages(messages.concat(message));
+    });
+
+    return () => {
+      // Use optional chaining to handle null case
+      socketRef.current?.disconnect();
+    };
+  }, []);
+
+  const sendMessage = () => {
+    if (socketRef.current && messageInput.trim() !== '') {
+      // Use optional chaining to handle null case
+      socketRef.current?.emit('message', messageInput);
+      console.log('message', messageInput);
+      setMessageInput('');
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <ul>
+          {messages.map((message, index) => (
+            <li key={index}>{message}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={messageInput}
+          onChange={(e) => setMessageInput(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+    </div>
+  );
+};
+
+export default TestChat;
