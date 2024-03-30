ca0123b10e92332cd0d59c353548dba2c3f25807
cyunlee <siyunkr@gmail.com>
2024-03-30 10:48:16 +0900

    kakao map

--- mind-mate/client/src/components/PlacesPage/Kakao.tsx
+++ mind-mate/client/src/components/PlacesPage/Kakao.tsx
@@ -6,10 +6,12 @@ declare global {
     }
   }
   
-  export default function Kakao() {
+  export default function Kakao(props: any) {
       const [map, setMap] = useState<any>();
-      const [marker, setMarker] = useState<any>();
-        
+      const [markers, setMarkers] = useState<any>([]);
+      const [ps, setPs] = useState<any>();
+      const [infowindow, setInfowindow] = useState({zindex:1});
+          
       useEffect(() => {
       window.kakao.maps.load(() => {
         const container = document.getElementById("map");
@@ -18,12 +20,22 @@ declare global {
           level: 3,
         };
         
+        //지도 생성
         setMap(new window.kakao.maps.Map(container, options));
-        setMarker(new window.kakao.maps.Marker());
+        //장소 검색 객체 생성
+        setPs(new window.kakao.maps.services.Places());
+        //검색 결과 목록이나 마커를 클릭했을 때 장소명을 표출할 인포윈도우 생성
+        setInfowindow(new window.kakao.maps.InfoWindow({zindex:1}));
+        
       });
     }, []);
     
-    return(<div id="map" style={{ width: "100%", height: "100vh"}}></div>)
+    return(
+    <>
+      <div id="map" style={{ width: "100%", height: "100vh"}}>
+      </div>
+    </>
+    )
   }
 
 
--- mind-mate/client/src/pages/PlacesPage.tsx
+++ mind-mate/client/src/pages/PlacesPage.tsx
@@ -1,14 +1,22 @@
 import Kakao from "../components/PlacesPage/Kakao";
 import TopBar from "../components/TopBar";
+import { useState } from "react";
+
 import '../styles/PlacesPage.scss';
 
 function PlacesPage() {
+
+    const [keywordVal, setKeywordVal] = useState<string>();
 
 
     return ( 
         <>
             <TopBar/>
-            <Kakao/>
+            <Kakao keyword={keywordVal}/>
+            <div className="keyword-search-container">
+                <button value="심리상담소" onClick={()=>{setKeywordVal("심리상담소")}}>심리상담소</button>
+                <button value="정신건강의학과" onClick={()=>{setKeywordVal("정신건강의학과")}}>정신건강의학과</button>
+            </div>
         </>
      );
 }
